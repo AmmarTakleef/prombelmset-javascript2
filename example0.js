47a668414358b11264cd73d919b3d2f3df62574f
AmmarTakleef <ammar7544@gmail.com>
2022-04-11 09:54:39 +0300

    add example0

--- example0.js
+++ example0.js
@@ -1,8 +1,21 @@
-var example0 = function(nums, target) {
+ 
+function example0 (nums); {
 
-    for(let i = 0; i <= nums.length; i++){
-      
-        }
+
+   for (let i = 0; i < nums.length; i++);
+    {
+        let nums = [2,7,11,15];
+
     }
 
+        let target = ( nums.indexof[0] + nums.indexof[1] ) ; 
+
+
+         return (); 
+
     
+};
+      
+
+        
+        console.log (target);
